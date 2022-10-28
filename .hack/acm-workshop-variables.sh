f517c26c97a90d91d8e836d7860657b2ced56897
Michael Bychkowski <mbychkowski@google.com>
2022-10-28 22:41:13 +0000

    Setup demo

--- .hack/acm-workshop-variables.sh
+++ .hack/acm-workshop-variables.sh
@@ -1,3 +1,12 @@
 export CONFIG_CONTROLLER_NAME=k8s-acm
 export CONFIG_CONTROLLER_LOCATION=northamerica-northeast1
 export CONFIG_CONTROLLER_NETWORK=main-vpc
+export ORG_REPO_URL=git@github.com:mbychkowski/gcp-acm-workshop-org.git
+export ORG_REPO_NAME_WITH_OWNER=mbychkowski/gcp-acm-workshop-org
+export GKE_NAME=first-k8s-acm-cluster
+export GKE_SA=gke-primary-pool
+export GKE_LOCATION=us-central1
+export GKE_CONFIGS_REPO_URL="https://github.com/mbychkowski/gcp-acm-workshop-gke-configs"
+export TENANT_PROJECT_ID=valinor-realm
+export TENANT_PROJECT_NUMBER=512806283726
+export TENANT_PROJECT_SA_EMAIL=valinor-realm@valinor-realm.iam.gserviceaccount.
