70301fe6f9c4e2c16dd3403f1b33d1b5b4d959f0
Michael Bychkowski <mbychkowski@google.com>
2022-10-29 21:57:48 +0000

    Add test test env

---
+++ .hack/acm-workshop-variables-1026614741785.sh
@@ -0,0 +1,19 @@
+export CONFIG_CONTROLLER_NAME=k8s-acm
+export CONFIG_CONTROLLER_LOCATION=northamerica-northeast1
+export CONFIG_CONTROLLER_NETWORK=vpc-iad-net
+export CONFIG_CONTROLLER_SA=service-1026614741785@gcp-sa-yakima.iam.gserviceaccount.com
+
+export ORG_REPO_URL=git@github.com:mbychkowski/gcp-acm-workshop-org.git
+export ORG_REPO_NAME_WITH_OWNER=mbychkowski/gcp-acm-workshop-org
+
+export GKE_NAME=first-k8s-acm-cluster
+export GKE_SA=gke-primary-pool
+export GKE_LOCATION=us-central1
+export GKE_PLATFORM_REPO_URL=git@github.com:mbychkowski/gcp-acm-workshop-org.git
+export GKE_CONFIGS_REPO_URL="https://github.com/mbychkowski/gcp-acm-workshop-gke-configs"
+
+# Unique. Check these
+export HOST_PROJECT_ID=kowski-iad-core
+export TENANT_PROJECT_ID=kowski-iad-core
+export TENANT_PROJECT_NUMBER=1026614741785
+export TENANT_PROJECT_SA_EMAIL=kowski-iad-core@kowski-iad-core.iam.gserviceaccount.
